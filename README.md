9c9819d9525acbfc038b5dfe0b66744be67e320d
TimChen <a5040766@gmail.com>
2023-01-03 23:53:26 +0800

    v5 to Twitch API Migration
    v5 to Twitch API Migration

--- README.md
+++ README.md
@@ -12,4 +12,9 @@
 ### Project 說明
 
 使用bootstrap簡單切版  
-並分別透過原生JavaScipt、JQuery Ajax和axios 分別串接Twitch API並展示實況頁面，並支援不同語言(中文版、英文版)
+並分別透過axios 串接Twitch API並展示實況頁面，並支援不同語言(中文版、英文版)
+
+--
+2023/01/23更新
+由於舊版Twitch v5 API不支援故升級至新的 Twitch API
+並更新jQuery和bootstrap版本
--- script.js
+++ script.js
@@ -1,130 +1,154 @@
-let clientId = '7jnow7dr6xs9f979a40a1rbd3rlim6';
-let LANG = 'zh';
+let clientId = "sa8qsftf1cnffx3n1uez5gg39twnvv";
+let clinetSecret = "co3qq6jzlwpck7je27ncevo8j1muow";
+let LANG = "zh";
 
-//XMLHttpRequest
-function getLoLData(LANG) {
-    clearData();
-    let apiUrl = `https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&limit=6&language=${LANG}`;
-    let request = new XMLHttpRequest();
-    request.open('GET', apiUrl, true);//非同步
-    request.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
-    request.setRequestHeader('Client-ID', clientId);
-    request.send();
-    request.onload = function load() {
-        if (this.status >= 200 && this.status < 400) {
-            let data = JSON.parse(this.responseText);
-            getData(data, block1);
-
-        } else {
-            console.log('Error');
-        }
-    };
-    request.onerror = function error() {
-        console.log('Error');
-    };
-    $('#LolBlock').show();
-    $('#MSBlock').hide();
-    $('#ChattingBlock').hide();
-};
-
-//JQuery AJAX
-function getMSData(LANG) {
-    clearData();
-    let apiUrl = `https://api.twitch.tv/kraken/streams/?game=MapleStory&limit=6&language=${LANG}`;
-    // AJAX寫法1
-    // $.ajax({
-    //     url: apiUrl,
-    //     headers: {
-    //         'Accept': 'application/vnd.twitchtv.v5+json',
-    //         'client-ID': clientId,
-    //     },
-    //     success: (data) => {
-    //         getData(data, block2);
-    //     },
-    //     error: function () {
-    //         console.log('JQuery Ajax Error');
-    //     }
-    // })
-
-    // AJAX寫法2
-    $.ajax({
-        url: apiUrl,
-        headers: {
-            'Accept': 'application/vnd.twitchtv.v5+json',
-            'client-ID': clientId,
-        }
-    }).done(data => getData(data, block2))
-        .fail(() => console.log('JQuery Ajax Error'))
-        .always(() => console.log('complete'));
-
-    $('#LolBlock').hide();
-    $('#MSBlock').show();
-    $('#ChattingBlock').hide();
-};
-
-//axios
-function getChattingData(LANG) {
-    clearData();
-    let apiUrl = `https://api.twitch.tv/kraken/streams/?game=Just%20Chatting&limit=6&language=${LANG}`;
-
-    axios.get(apiUrl, {
-        headers: {
-            'Accept': 'application/vnd.twitchtv.v5+json',
-            'client-ID': clientId,
-        }
-    }).then(res => getData(res.data, block3))
-        .catch(error => console.log(error));
-
-    $('#LolBlock').hide();
-    $('#MSBlock').hide();
-    $('#ChattingBlock').show();
+async function getTwitchAuthorization() {
+  let url = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clinetSecret}&grant_type=client_credentials&redirect_uri=http://127.0.0.1:5500/`;
+  let response = await axios.post(url);
+  return response.data;
 }
 
+async function getLoLData(LANG) {
+  clearData();
+  const url = `https://api.twitch.tv/helix/streams?game_id=21779&first=12&language=${LANG}`;
 
-function getData(data, block) {
-    const streams = data.streams;
-    const $row = $(block);//選擇添加區塊
-    for (var i = 0; i < streams.length; i++) {
-        $row.append(getColumn(streams[i]))
-    }
-};
+  let authorizationObject = await getTwitchAuthorization();
+  let { access_token, expires_in, token_type } = authorizationObject;
+
+  //將bearer第一個字轉大寫
+  token_type =
+    token_type.substring(0, 1).toUpperCase() +
+    token_type.substring(1, token_type.length);
+
+  let authorization = `${token_type} ${access_token}`;
+  let headers = {
+    Authorization: authorization,
+    "Client-Id": clientId,
+  };
+
+  axios
+    .get(url, {
+      headers: headers,
+    })
+    .then((response) => {
+      let streams = response.data.data;
+      getData(streams, block1);
+    });
+
+  $("#LolBlock").show();
+  $("#MSBlock").hide();
+  $("#ChattingBlock").hide();
+}
+
+async function getMSData(LANG) {
+  clearData();
+  const url = `https://api.twitch.tv/helix/streams?game_id=19976&first=12&language=${LANG}`;
+  let authorizationObject = await getTwitchAuthorization();
+  let { access_token, expires_in, token_type } = authorizationObject;
+
+  //將bearer第一個字轉大寫
+  token_type =
+    token_type.substring(0, 1).toUpperCase() +
+    token_type.substring(1, token_type.length);
+
+  let authorization = `${token_type} ${access_token}`;
+  let headers = {
+    Authorization: authorization,
+    "Client-Id": clientId,
+  };
+
+  axios
+    .get(url, {
+      headers: headers,
+    })
+    .then((response) => {
+      let streams = response.data.data;
+      getData(streams, block2);
+    });
+
+  $("#LolBlock").hide();
+  $("#MSBlock").show();
+  $("#ChattingBlock").hide();
+}
+
+async function getChattingData(LANG) {
+  clearData();
+  const url = `https://api.twitch.tv/helix/streams?game_id=509658&first=12&language=${LANG}`;
+  let authorizationObject = await getTwitchAuthorization();
+  let { access_token, expires_in, token_type } = authorizationObject;
+
+  //將bearer第一個字轉大寫
+  token_type =
+    token_type.substring(0, 1).toUpperCase() +
+    token_type.substring(1, token_type.length);
+
+  let authorization = `${token_type} ${access_token}`;
+  let headers = {
+    Authorization: authorization,
+    "Client-Id": clientId,
+  };
+
+  axios
+    .get(url, {
+      headers: headers,
+    })
+    .then((response) => {
+      let streams = response.data.data;
+      getData(streams, block3);
+    });
+
+  $("#LolBlock").hide();
+  $("#MSBlock").hide();
+  $("#ChattingBlock").show();
+}
+
+function getData(streams, block) {
+  console.log(streams);
+  const $row = $(block); //選擇添加區塊
+  for (var i = 0; i < streams.length; i++) {
+    $row.append(getColumn(streams[i]));
+  }
+}
 
 function getColumn(data) {
-    return `<div class="col-12 col-lg-6 col-xl-4">
-            <a href="${data.channel.url}" target="_blank" class="twitch_block">
+  console.log(data);
+  data.thumbnail_url = data.thumbnail_url.replace(
+    "{width}x{height}",
+    "320x220"
+  );
+
+  data.avatar='https://static-cdn.jtvnw.net/previews-ttv/live_user_namin1004-70x70.jpg'
+
+  return `<div class="col-12 col-lg-6 col-xl-4">
+            <a href="https://www.twitch.tv/${data.user_login}" target="_blank" class="twitch_block">
                     <div class="twitch_preview">
-                        <img src="${data.preview.medium}"/>
-                    <div class="twitch_info">
-                        <img src="${data.channel.logo}" class="twitch_avatar"/>
-                        <div class="ml-2">
-                                <div class="twitch_title">${data.channel.status}</div>
+                        <img src="${data.thumbnail_url}"/>
+                    <div class="p-1">
+                        <div class="ms-2">
+                                <div class="twitch_title">${data.title}</div>
                             <div class="twitch_name">
-                                <div class="float-left">${data.channel.display_name} (${data.channel.name})</div>
-                                <div class="float-right text-danger"><i class="fa fa-user"></i> ${data.viewers}</div>
+                                <div class="float-left">${data.user_name} (${data.user_login})</div>
+                                <div class="float-right text-danger"><i class="fa fa-user"></i> ${data.viewer_count}</div>
                             </div>
                         </div>
                     </div>
                 </div>
             </a>
         </div>`;
-};
+}
 
 function clearData() {
-    $('.row').empty();//清除元素裡的內容
+  $(".row").empty(); //清除元素裡的內容
 }
 
 function changeLang(lang, type) {
-    $('.container h3').text(window.I18N[lang]['TITLE']);
-    $('#LOL').text(window.I18N[lang]['LOLNAME']);
-    $('#MS').text(window.I18N[lang]['MSNAME']);
-    $('#Chat').text(window.I18N[lang]['ChattingNAME']);
-    $('.row').empty();//清除元素裡的內容
+  $(".container h3").text(window.I18N[lang]["TITLE"]);
+  $("#LOL").text(window.I18N[lang]["LOLNAME"]);
+  $("#MS").text(window.I18N[lang]["MSNAME"]);
+  $("#Chat").text(window.I18N[lang]["ChattingNAME"]);
+  $(".row").empty(); //清除元素裡的內容
 
-    if (type == 'LOL')
-        getLoLData(lang);
-    if (type == 'MS')
-        getMSData(lang);
-    if (type == 'Chatting')
-        getChattingData(lang);
-
-};
+  if (type == "LOL") getLoLData(lang);
+  if (type == "MS") getMSData(lang);
+  if (type == "Chatting") getChattingData(lang);
+}
